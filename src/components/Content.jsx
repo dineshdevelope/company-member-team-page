193630a67b68b25b4be06576667696c4fcd85fac
dineshdevelope <dineshindevelope@gmail.com>
2024-04-05 03:27:51 +0530

    header-completed

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -14,6 +14,24 @@ const Content = () => {
         who make our company thrive.
       </p>
       <hr />
+
+      <h1 className="  w-fit p-2 m-3 sm:p-5   mx-auto sm:mx-auto font-semibold text-black  sm:mt-1 text-lg sm:text-xl">
+        Meet Our Team
+      </h1>
+
+      <h1 className="bg-gray-300 hover:bg-blue-700 w-fit p-2 m-2 sm:p-3 rounded-lg  mx-auto sm:mx-auto font-semibold text-blue-500 hover:text-white sm:mt-1 text-xl sm:text-2xl">
+        Leadership / Management Team
+      </h1>
+      <main className="max-w-md mx-auto mt-5">
+        <img
+          src="https://avatars.githubusercontent.com/u/15264938?v=4"
+          alt="AnbuRocky-Photo"
+          className="w-48 rounded-lg"
+          title="Mr.AnbuSelvan - CEO"
+        />
+        <h1 className="font-semibold mt-2 text-red-600"> Mr.AnbuSelvan</h1>
+        <h1 className="font-semibold  text-blue-600">Founder / CEO</h1>
+      </main>
     </div>
   );
 };
