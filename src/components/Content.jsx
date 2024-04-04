b59e12ce0203112d2a07e326678ec766299f81a4
dineshdevelope <dineshindevelope@gmail.com>
2024-04-05 02:57:10 +0530

    init-project

---
+++ src/components/Content.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Content = () => {
+  return (
+    <div>
+      <h1 className="bg-gray-300 hover:bg-blue-700 w-fit p-2 m-5 sm:p-5 rounded-lg  mx-auto sm:mx-auto font-semibold text-blue-500 hover:text-white sm:m-10 text-xl sm:text-3xl">
+        Cyber Dude Networks
+      </h1>
+
+      <p className="text-center font-light w-full mb-5">
+        Welcome to Cyber Dude Networks Pvt Ltd.., where a dedicated team of
+        professionals works <br></br>together to bring innovation, creaativity
+        and expertise to software industry get to know <br></br> the individuals
+        who make our company thrive.
+      </p>
+      <hr />
+    </div>
+  );
+};
+
+export default Content;
