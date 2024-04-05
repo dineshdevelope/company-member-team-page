522edbb56b1a3d9ab338054d9395c8a4f80ccd7c
dineshdevelope <dineshindevelope@gmail.com>
2024-04-06 00:48:39 +0530

    init

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Content = () => {
   return (
     <div>
-      <h1 className="bg-gray-300 hover:bg-blue-700 w-fit p-2 m-5 sm:p-5 rounded-lg  mx-auto sm:mx-auto font-semibold text-blue-500 hover:text-white sm:m-10 text-xl sm:text-3xl">
+      <h1 className="bg-gray-300 hover:bg-blue-700 w-fit p-2 m-5 sm:p-4 rounded-lg  mx-auto sm:mx-auto font-semibold text-blue-500 hover:text-white sm:m-7 text-xl sm:text-3xl">
         Cyber Dude Networks
       </h1>
 
@@ -15,22 +15,241 @@ const Content = () => {
       </p>
       <hr />
 
-      <h1 className="  w-fit p-2 m-3 sm:p-5   mx-auto sm:mx-auto font-semibold text-black  sm:mt-1 text-lg sm:text-xl">
+      <h1 className="  w-fit p-2 m-3 sm:p-3   mx-auto sm:mx-auto font-semibold text-black  sm:mt-1 text-lg sm:text-xl">
         Meet Our Team
       </h1>
 
       <h1 className="bg-gray-300 hover:bg-blue-700 w-fit p-2 m-2 sm:p-3 rounded-lg  mx-auto sm:mx-auto font-semibold text-blue-500 hover:text-white sm:mt-1 text-xl sm:text-2xl">
         Leadership / Management Team
       </h1>
-      <main className="max-w-md mx-auto mt-5">
-        <img
-          src="https://avatars.githubusercontent.com/u/15264938?v=4"
-          alt="AnbuRocky-Photo"
-          className="w-48 rounded-lg"
-          title="Mr.AnbuSelvan - CEO"
-        />
-        <h1 className="font-semibold mt-2 text-red-600"> Mr.AnbuSelvan</h1>
-        <h1 className="font-semibold  text-blue-600">Founder / CEO</h1>
+      <main className=" mt-6 ">
+        <div className="sm:flex justify-around  mx-auto max-w-xl">
+          <div className="flex flex-col items-center">
+            <img
+              src="https://avatars.githubusercontent.com/u/15264938?v=4"
+              alt="AnbuRocky-Photo"
+              className="w-40 rounded-full"
+              title="Mr.AnbuSelvan - CEO"
+            />
+
+            <h1 className="font-semibold mt-2 text-red-600"> Mr.AnbuSelvan</h1>
+            <h1 className="font-semibold  text-blue-600">Founder / CEO</h1>
+          </div>
+          <div className="flex flex-col items-center">
+            <img
+              src="https://avatars.githubusercontent.com/u/15264938?v=4"
+              alt="AnbuRocky-Photo"
+              className="w-40 rounded-full"
+              title="Mr.AnbuSelvan - CEO"
+            />
+
+            <h1 className="font-semibold mt-2 text-red-600"> Mr.AnbuSelvan</h1>
+            <h1 className="font-semibold  text-blue-600">Founder / CEO</h1>
+          </div>
+        </div>
+        <div className="sm:flex justify-around  mx-auto max-w-3xl mt-10">
+          <div className="flex flex-col items-center">
+            <img
+              src="https://avatars.githubusercontent.com/u/15264938?v=4"
+              alt="AnbuRocky-Photo"
+              className="w-40 rounded-full"
+              title="Mr.AnbuSelvan - CEO"
+            />
+
+            <h1 className="font-semibold mt-2 text-red-600"> Mr.AnbuSelvan</h1>
+            <h1 className="font-semibold  text-blue-600">Founder / CEO</h1>
+          </div>
+          <div className="flex flex-col items-center">
+            <img
+              src="https://avatars.githubusercontent.com/u/15264938?v=4"
+              alt="AnbuRocky-Photo"
+              className="w-40 rounded-full"
+              title="Mr.AnbuSelvan - CEO"
+            />
+
+            <h1 className="font-semibold mt-2 text-red-600"> Mr.AnbuSelvan</h1>
+            <h1 className="font-semibold  text-blue-600">Founder / CEO</h1>
+          </div>
+          <div className="flex flex-col items-center">
+            <img
+              src="https://avatars.githubusercontent.com/u/15264938?v=4"
+              alt="AnbuRocky-Photo"
+              className="w-40 rounded-full"
+              title="Mr.AnbuSelvan - CEO"
+            />
+
+            <h1 className="font-semibold mt-2 text-red-600"> Mr.AnbuSelvan</h1>
+            <h1 className="font-semibold  text-blue-600">Founder / CEO</h1>
+          </div>
+        </div>
+        <h1 className="bg-gray-300 hover:bg-blue-700 w-fit p-2 m-2 sm:p-3 rounded-lg  mx-auto sm:mx-auto font-semibold text-blue-500 hover:text-white sm:mt-7 text-xl sm:text-2xl">
+          Developement Team
+        </h1>
+        <div className="sm:flex justify-around  mx-auto max-w-4xl  mt-7">
+          <div className="flex flex-col items-center">
+            <img
+              src="https://avatars.githubusercontent.com/u/15264938?v=4"
+              alt="AnbuRocky-Photo"
+              className="w-40 rounded-full"
+              title="Mr.AnbuSelvan - CEO"
+            />
+
+            <h1 className="font-semibold mt-2 text-red-600"> Mr.AnbuSelvan</h1>
+            <h1 className="font-semibold  text-blue-600">Founder / CEO</h1>
+          </div>
+          <div className="flex flex-col items-center ">
+            <img
+              src="https://avatars.githubusercontent.com/u/15264938?v=4"
+              alt="AnbuRocky-Photo"
+              className="w-40 rounded-full"
+              title="Mr.AnbuSelvan - CEO"
+            />
+
+            <h1 className="font-semibold mt-2 text-red-600"> Mr.AnbuSelvan</h1>
+            <h1 className="font-semibold  text-blue-600">Founder / CEO</h1>
+          </div>
+          <div className="flex flex-col items-center">
+            <img
+              src="https://avatars.githubusercontent.com/u/15264938?v=4"
+              alt="AnbuRocky-Photo"
+              className="w-40 rounded-full"
+              title="Mr.AnbuSelvan - CEO"
+            />
+
+            <h1 className="font-semibold mt-2 text-red-600"> Mr.AnbuSelvan</h1>
+            <h1 className="font-semibold  text-blue-600">Founder / CEO</h1>
+          </div>
+          <div className="flex flex-col items-center ">
+            <img
+              src="https://avatars.githubusercontent.com/u/15264938?v=4"
+              alt="AnbuRocky-Photo"
+              className="w-40 rounded-full"
+              title="Mr.AnbuSelvan - CEO"
+            />
+
+            <h1 className="font-semibold mt-2 text-red-600"> Mr.AnbuSelvan</h1>
+            <h1 className="font-semibold  text-blue-600">Founder / CEO</h1>
+          </div>
+        </div>
+        <div className="sm:flex justify-around  mx-auto max-w-4xl  mt-7">
+          <div className="flex flex-col items-center">
+            <img
+              src="https://avatars.githubusercontent.com/u/15264938?v=4"
+              alt="AnbuRocky-Photo"
+              className="w-40 rounded-full"
+              title="Mr.AnbuSelvan - CEO"
+            />
+
+            <h1 className="font-semibold mt-2 text-red-600"> Mr.AnbuSelvan</h1>
+            <h1 className="font-semibold  text-blue-600">Founder / CEO</h1>
+          </div>
+          <div className="flex flex-col items-center ">
+            <img
+              src="https://avatars.githubusercontent.com/u/15264938?v=4"
+              alt="AnbuRocky-Photo"
+              className="w-40 rounded-full"
+              title="Mr.AnbuSelvan - CEO"
+            />
+
+            <h1 className="font-semibold mt-2 text-red-600"> Mr.AnbuSelvan</h1>
+            <h1 className="font-semibold  text-blue-600">Founder / CEO</h1>
+          </div>
+          <div className="flex flex-col items-center">
+            <img
+              src="https://avatars.githubusercontent.com/u/15264938?v=4"
+              alt="AnbuRocky-Photo"
+              className="w-40 rounded-full"
+              title="Mr.AnbuSelvan - CEO"
+            />
+
+            <h1 className="font-semibold mt-2 text-red-600"> Mr.AnbuSelvan</h1>
+            <h1 className="font-semibold  text-blue-600">Founder / CEO</h1>
+          </div>
+        </div>
+        <h1 className="bg-gray-300 hover:bg-blue-700 w-fit p-2 m-2 sm:p-3 rounded-lg  mx-auto sm:mx-auto font-semibold text-blue-500 hover:text-white sm:mt-7 text-xl sm:text-2xl">
+          Sales and Marketing Team
+        </h1>
+        <div className="sm:flex justify-around  mx-auto max-w-4xl  mt-7">
+          <div className="flex flex-col items-center">
+            <img
+              src="https://avatars.githubusercontent.com/u/15264938?v=4"
+              alt="AnbuRocky-Photo"
+              className="w-40 rounded-full"
+              title="Mr.AnbuSelvan - CEO"
+            />
+
+            <h1 className="font-semibold mt-2 text-red-600"> Mr.AnbuSelvan</h1>
+            <h1 className="font-semibold  text-blue-600">Founder / CEO</h1>
+          </div>
+          <div className="flex flex-col items-center ">
+            <img
+              src="https://avatars.githubusercontent.com/u/15264938?v=4"
+              alt="AnbuRocky-Photo"
+              className="w-40 rounded-full"
+              title="Mr.AnbuSelvan - CEO"
+            />
+
+            <h1 className="font-semibold mt-2 text-red-600"> Mr.AnbuSelvan</h1>
+            <h1 className="font-semibold  text-blue-600">Founder / CEO</h1>
+          </div>
+          <div className="flex flex-col items-center">
+            <img
+              src="https://avatars.githubusercontent.com/u/15264938?v=4"
+              alt="AnbuRocky-Photo"
+              className="w-40 rounded-full"
+              title="Mr.AnbuSelvan - CEO"
+            />
+
+            <h1 className="font-semibold mt-2 text-red-600"> Mr.AnbuSelvan</h1>
+            <h1 className="font-semibold  text-blue-600">Founder / CEO</h1>
+          </div>
+          <div className="flex flex-col items-center ">
+            <img
+              src="https://avatars.githubusercontent.com/u/15264938?v=4"
+              alt="AnbuRocky-Photo"
+              className="w-40 rounded-full"
+              title="Mr.AnbuSelvan - CEO"
+            />
+
+            <h1 className="font-semibold mt-2 text-red-600"> Mr.AnbuSelvan</h1>
+            <h1 className="font-semibold  text-blue-600">Founder / CEO</h1>
+          </div>
+        </div>
+        <div className="sm:flex justify-around  mx-auto max-w-4xl  mt-7">
+          <div className="flex flex-col items-center">
+            <img
+              src="https://avatars.githubusercontent.com/u/15264938?v=4"
+              alt="AnbuRocky-Photo"
+              className="w-40 rounded-full"
+              title="Mr.AnbuSelvan - CEO"
+            />
+
+            <h1 className="font-semibold mt-2 text-red-600"> Mr.AnbuSelvan</h1>
+            <h1 className="font-semibold  text-blue-600">Founder / CEO</h1>
+          </div>
+          <div className="flex flex-col items-center ">
+            <img
+              src="https://avatars.githubusercontent.com/u/15264938?v=4"
+              alt="AnbuRocky-Photo"
+              className="w-40 rounded-full"
+              title="Mr.AnbuSelvan - CEO"
+            />
+
+            <h1 className="font-semibold mt-2 text-red-600"> Mr.AnbuSelvan</h1>
+            <h1 className="font-semibold  text-blue-600">Founder / CEO</h1>
+          </div>
+          <div className="flex flex-col items-center">
+            <img
+              src="https://avatars.githubusercontent.com/u/15264938?v=4"
+              alt="AnbuRocky-Photo"
+              className="w-40 rounded-full"
+              title="Mr.AnbuSelvan - CEO"
+            />
+
+            <h1 className="font-semibold mt-2 text-red-600"> Mr.AnbuSelvan</h1>
+            <h1 className="font-semibold  text-blue-600">Founder / CEO</h1>
+          </div>
+        </div>
       </main>
     </div>
   );
